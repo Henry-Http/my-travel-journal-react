256a8fc5c1bbed3604a46becc5db1eee3598487f
HoD <henrybrownonline@yahoo.com>
2023-03-19 00:37:59 +0100

    commit 3

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,16 +3,18 @@ import React from "react";
 export default function Body(props) {
   return (
     <div className="main-body">
-      <img src={props.coverImg} className="contentImg" ></img>
+      <img src={props.coverImg} className="contentImg"></img>
       <div className="location">
         <img src={props.locationImg} className="location-icon"></img>
         <p>{props.location.toUpperCase()}</p>
-        <a href=""><p className="map-text">{props.maps}</p></a>
+        <a href="https://www.google.com/maps">
+          <p className="map-text">{props.maps}</p>
+        </a>
       </div>
       <p className="item-title">{props.title}</p>
       <div className="date-info">
-      <p className="item-date">{props.date}</p>
-      <p className="item-info">{props.info}</p>
+        <p className="item-date">{props.date}</p>
+        <p className="item-info">{props.info}</p>
       </div>
     </div>
   );
